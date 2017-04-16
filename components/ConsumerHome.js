d8b094f0c324cf93ed8a6fd3e21e95097f44d18c
lope391 <lope1996@gmail.com>
2017-04-16 17:50:46 -0500

    UI Dev branch, Consumer Home

--- components/ConsumerHome.js
+++ components/ConsumerHome.js
@@ -1,3 +1,13 @@
 /**
  * Created by lope1 on 4/16/2017.
  */
+
+import React, { Component } from 'react';
+import { Container, Content, Body, Left, Right, Title, Header, Icon, Form, Input, Item, Button, Text } from 'native-base';
+import { Image } from 'react-native'
+import { Col, Row, Grid } from 'react-native-easy-grid';
+
+class ConsumerHome extends Component {
+
+
+}
