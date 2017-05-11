8b847e8c0cf41cb7d9b251e7fc3db6b9888751e0
dbenite2 <flio034@gmail.com>
2017-05-11 16:32:53 -0500

    consumer side bar agregado

---
+++ components/ConsumerSideBar.js
@@ -0,0 +1,83 @@
+import React, {Component} from 'react';
+import { Container, Content, Body, Left, Right, Title, Header, Icon, Form, Item, Button, Text
+    ,List, ListItem } from 'native-base';
+import { Image } from 'react-native'
+import { Col, Row, Grid } from 'react-native-easy-grid';
+
+class ConsumerSideBar extends Component {
+
+    static propTypes = {};
+    static defaultProps = {};
+
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+
+_navigate(){
+    this.props.navigator.push({
+        name: 'SideBar' , // Matches route.name
+    })
+}
+
+render(){
+    return( 
+        <Container style={{backgroundColor:"#BDBDBD"}}>
+            <Header style={{backgroundColor:"#9C27B0",height:80}}>
+                <Body alignItems="flex-end">
+                    <Title style={{color: '#FFFFFF'}}>Locketz</Title>
+                </Body>
+            </Header>
+        <Content>
+            <List>
+             <ListItem>
+               <Button transparent block style={{height:30,width:140}}
+                    onPress={
+                        () => {
+                            this.props.navigator.push({id: 'ConsumerHome'})
+                              }
+                            }
+                            >
+                   <Left>
+                        <Text>Home</Text>
+                   </Left>
+                        
+               </Button>
+             </ListItem>
+
+             <ListItem>
+                <Button transparent block style={{height:30,width:140}}
+                    onPress={
+                        () => {
+                            this.props.navigator.push({id: 'settings'})
+                              }
+                            }
+                    >
+                   <Left>
+                        <Text>Settings</Text>
+                   </Left>
+                </Button>
+             </ListItem>
+
+             <ListItem>
+                <Buttom transparent block style={{height:30,width:140}}
+                    onPress={
+                        () => {
+                            this.props.navigator.push({id: 'Search'})
+                              }
+                            }
+                  >  
+                  <Left>
+                      <Text>Search</Text>
+                  </Left>
+
+            </Buttom>
+        </ListItem> 
+    </List>
+   </Content>
+</Container>
+    )
+   }
+}
+export default ConsumerSideBar
