fae52d20dff33a20dd67341c6e21fe67e7e66d82
simongomez95 <simongomez95@gmail.com>
2017-06-02 03:25:14 -0500

    paypal

--- android/app/src/main/java/com/locketz/MainActivity.java
+++ android/app/src/main/java/com/locketz/MainActivity.java
@@ -1,6 +1,9 @@
 package com.locketz;
 
 import com.facebook.react.ReactActivity;
+import br.com.vizir.rn.paypal.PayPalPackage; // <--
+import br.com.vizir.rn.paypal.PayPal; // <--
+import android.content.Intent; // <--
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +15,38 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "locketz";
     }
+
+     private static final int PAY_PAL_REQUEST_ID = 9; // <-- Can be any unique number
+        private PayPalPackage payPalPackage; // <--
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            // ...
+            payPalPackage = new PayPalPackage(this, PAY_PAL_REQUEST_ID); // <--
+
+            mReactInstanceManager = ReactInstanceManager.builder()
+                    .setApplication(getApplication())
+                    .setBundleAssetName("index.android.bundle")
+                    .setJSMainModuleName("index.android")
+                    .addPackage(new MainReactPackage())
+                    // ...
+                    .addPackage(payPalPackage) // <--
+                    .setUseDeveloperSupport(BuildConfig.DEBUG)
+                    .setInitialLifecycleState(LifecycleState.RESUMED)
+                    .build();
+            // ...
+        }
+
+        // ...
+
+        @Override
+        public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+           super.onActivityResult(requestCode, resultCode, data);
+           if (requestCode == PAY_PAL_REQUEST_ID) {
+               payPalPackage.handleActivityResult(requestCode, resultCode, data); // <--
+           } else {
+               otherModulesHandlers(requestCode, resultCode, data);
+           }
+        }
 }
